426e5f0c6555e161a74de414d0ac8937ac491027
Habib Frambudi <habibframbudi@gmail.com>
2026-04-12 10:07:31 +0700

    Update About page and version to v2.17.0

--- includes/version.php
+++ includes/version.php
@@ -3,7 +3,13 @@
  * IPManager Pro Version Information
  */
 
-if (!defined('APP_VERSION')) define('APP_VERSION', '2.15.1');
-if (!defined('APP_RELEASE_DATE')) define('APP_RELEASE_DATE', '2026-04-09');
+if (!defined('APP_VERSION')) define('APP_VERSION', '2.17.0');
+if (!defined('APP_RELEASE_DATE')) define('APP_RELEASE_DATE', '2026-04-12');
 if (!defined('GITHUB_REPO')) define('GITHUB_REPO', 'frambudi75/IP-Manage');
 if (!defined('GITHUB_URL')) define('GITHUB_URL', 'https://github.com/' . GITHUB_REPO);
+
+$versions = [
+    ['ver' => '2.17.0', 'date' => '2026-04-12', 'changes' => ['Premium Visualization (Gradients & Gridless)', 'Professional Network Reports Module', 'Interactive Progress Tracking']],
+    ['ver' => '2.16.0', 'date' => '2026-04-12', 'changes' => ['Global Responsive Refactor (Mobile-First)', 'Standardized CSS utility classes', 'Topology Map Responsive Fix']],
+    ['ver' => '2.15.1', 'date' => '2026-04-09', 'changes' => ['Internal Bug Reporting System', 'Automated Activation Telemetry', 'Fixed .htaccess API routing']],
+];
